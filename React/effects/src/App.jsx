980fc7b05183c78b0150a2949f72b241633662cf
Ricardo Ordorica <ricaor82@gmail.com>
2025-12-10 12:51:29 -0600

    novena

---
+++ React/effects/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+import Counter from './Counter'
+import QuoteFetcher from './QuoteFetcher'
+import QuoteFetcherLoader from './QuoteFetcherLoader'
+import ProfileViewerWithSearch from './ProfileViewerWithSearch'
+
+function App() {
+
+  return (
+    <>
+      {/* <Counter /> */}
+      {/* <QuoteFetcher /> */}
+      {/* <QuoteFetcherLoader /> */}
+      <ProfileViewerWithSearch />
+    </>
+  )
+}
+
+export default App
